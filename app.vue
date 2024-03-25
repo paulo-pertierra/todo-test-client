8fe0fdaa7cd30ca4d5bb2be2937f4c50ad3a7aab
Paulo Pertierra <paulopertierra@gmail.com>
2024-03-25 15:33:56 +0800

    :rocket: first commit!

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ response }}
+  </div>
+</template>
+<script lang="ts" setup>
+const query = gql`
+query tasks {
+  tasks {
+    id
+    todo
+    done
+  }
+}
+`
+const response = await useAsyncQuery(query)
+</script>
