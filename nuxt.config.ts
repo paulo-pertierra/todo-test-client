af56c341c9d2cba2254dc5c5354c710c7aa7d324
Paulo Pertierra <paulopertierra@gmail.com>
2024-03-25 15:37:07 +0800

    feat: add vuetify plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,31 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/apollo'],
+  modules: [
+    '@nuxtjs/apollo',
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
   apollo: {
     clients: {
       default: {
         httpEndpoint: 'http://localhost:80/graphql'
       }
     }
-  }
+  },
+  build: {
+    transpile: ['vuetify']
+  },
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 })
---
+++ plugins/vuetify.ts
@@ -0,0 +1,9 @@
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const vuetify = createVuetify();
+  nuxtApp.vueApp.use(vuetify);
+})
